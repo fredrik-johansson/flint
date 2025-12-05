ef87c358b8fbc050d7c656f1bedce19ad2b61579
Fredrik Johansson <fredrik.johansson@gmail.com>
2025-12-05 18:11:53 +0100

    test2

--- src/ulong_extras/is_prime.c
+++ src/ulong_extras/is_prime.c
@@ -143,7 +143,7 @@ _n_is_strong_probabprime_redc(ulong a, ulong d, ulong one_red, nmod_redc_ctx_t c
     FLINT_ASSERT(a >= 2);
     FLINT_ASSERT(a != n - 1);
 
-    if (nmod_redc_can_use_fast(ctx))
+    if (nmod_redc_can_use_fast(ctx) && 0)
     {
         if (a == 2 && 0)
             y = _nmod_redc_fast_2_pow_ui(t, ctx);
